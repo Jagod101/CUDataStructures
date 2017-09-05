491ff76c5a0a8b7a649dbd871885637fb66f8dd3
jagod101 <jagod101@mail.chapman.edu>
2017-09-05 15:03:38 -0700

    Assignment 0 (Hello World)

---
+++ Assignment0/README.txt
@@ -0,0 +1,15 @@
+Zachary Jagoda
+Student ID: 2274813
+Student Email: jagod101@mail.chapman.edu
+CPSC 350-02
+Assignment 0
+Source Files: helloWorld.cpp 
+
+No runtime errors
+No references or peers used in completing this assignment
+
+Instructions: 
+	-Navigate to the file using 'cd' and directory names
+	-Compile the file with the command 'g++ helloWorld.cpp'
+		* this will generate the file a.out 
+	-Run the file with the command './a.out'
---
+++ Assignment0/helloWorld.cpp
@@ -0,0 +1,22 @@
+/*Zachary Jagoda
+  Student ID: 2274813
+  Student Email: jagod101@mail.chapman.edu
+  CPSC 350-02
+  Assignment 0 (Hello World)
+
+  The following assignment is to demonstrate C++ code working in the most basic program one can start by writing, the default Hello World script
+
+  The program insludes the following:
+	- 'using namespace std' to simplify code throughout and remove the requirement of 'std::'
+	- A singular main method that prints out the text "Hello World!"
+	- '\n' is included in the print statement to add an extra line to the console
+
+*/
+
+#include <iostream>
+
+using namespace std; //simplifies code throughout the program
+
+int main() {
+	cout << "Hello World!\n"; // '\n' prints an extra line after the statement
+}
