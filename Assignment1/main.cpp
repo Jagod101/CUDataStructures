19eacc489b9ca764741568b7dfd4bea282fb2b39
jagod101 <jagod101@mail.chapman.edu>
2017-09-06 20:06:05 -0700

    Assignment 1 created necessary files

---
+++ Assignment1/main.cpp
@@ -0,0 +1,16 @@
+/*Zachary Jagoda
+  Student ID: 2274813
+  Student Email: jagod101@mail.chapman.edu
+  CPSC 350-02
+  Assignment 1 (C++ Review/DNA)
+
+  The following assignment is to build a simple analysis program that will compute basic statistics for a list of DNA strings
+
+*/
+
+#include "assign1.h"
+
+#include <iostream>
+#include <math.h>
+#include <ofstream>
+#include <ifstream>
