f0f7d4e362f1ba7d734de5be18e096b64f018c03
Zach Jagoda <jagod101@mail.chapman.edu>
2017-10-03 15:08:53 -0700

    [UPDATE] Added Template Format

--- In-Class/Stacks/genstack.h
+++ In-Class/Stacks/genstack.h
@@ -1,17 +1,94 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
 class genstack
 {
     public:
         genstack(int maxsize);
         ~genstack();
 
-        void push(char data);
-        char pop();
-        char peek(); //top();
+        void push(T data);
+        T pop();
+        T peek(); //top();
         int isFull();
         int isEmpty();
 
         int top;
         int max;
 
-        char myArray;
+        T *myArray;
 };
+
+template <class T>
+genstack<T>::genstack(int maxsize)
+{
+    myArray = new T[maxsize];
+    max = maxsize;
+    top = -1; //Denotes that top is empty
+}
+
+template <class T>
+genstack<T>::~genstack()
+{
+    cout << "Object Destroyed" << endl;
+    //Also need to delete the Array
+}
+
+template <class T>
+void genstack<T>::push(T data)
+{
+    myArray[++top] = data;
+    //Add checks
+}
+
+template <class T>
+T genstack<T>::pop()
+{
+    //returns char in myArray from the top and then decrements
+    return myArray[top--];
+
+    //Implement error-checking for if myArray is empty
+}
+
+template <class T>
+T genstack<T>::peek()
+{
+
+    return myArray[top];
+}
+
+template <class T>
+int genstack<T>::isFull()
+{
+    return (top == max-1);
+}
+
+template <class T>
+int genstack<T>::isEmpty()
+{
+    return (top == -1);
+}
+
+/*
+    ORIGINAL genstack.h File for REFERENCE ONLY
+    
+    class genstack
+    {
+        public:
+            genstack(int maxsize);
+            ~genstack();
+
+            void push(char data);
+            char pop();
+            char peek(); //top();
+            int isFull();
+            int isEmpty();
+
+            int top;
+            int max;
+
+            char myArray;
+    };
+ */
--- In-Class/Stacks/main.cpp
+++ In-Class/Stacks/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main (int argc, char** argv) {
-    genstack g(5);
+    genstack<int> g(5);
 
     g.push("r");
     g.push("r");
@@ -24,4 +24,29 @@ int main (int argc, char** argv) {
     }
 
     return 0;
+
+    /*
+        ORIGINAL main.cpp file for REFERENCE
+        
+        genstack g(5);
+
+        g.push("r");
+        g.push("r");
+        g.push("r");
+        g.push("r");
+
+        cout << "POP: " << g.pop() << endl;
+        cout << "POP: " << g.pop() << endl;
+        cout << "PEEK: " << g.peek() << endl;
+        cout << "EMPTY: " << g.isEmpty() << endl;
+        
+        g.push("h");
+        g.push("L");
+
+        while(!g.isEmpty()) {
+            cout << g.pop() << endl;
+        }
+
+        return 0;
+    */
 }
