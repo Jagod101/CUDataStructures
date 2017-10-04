bb16f337a760cd6fb0317d6dbf37ca303b866eea
Zach Jagoda <jagod101@mail.chapman.edu>
2017-10-04 15:34:21 -0700

    [UPLOAD] File Template

--- Assignments/Assignment3/GenStack.h
+++ Assignments/Assignment3/GenStack.h
@@ -1,5 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
 class GenStack {
     private:
+
     public:
-        
+        GenStack();
+        ~GenStack();
+
+        void push();
+        T pop();
+        T peek();
+        int isFull();
+        int isEmpty();
+
+        void sourceCodePath();
+};
+
+template <class T>
+GenStack<T>::GenStack() {
+
+}
+
+template <class T>
+GenStack<T>::~GenStack() {
+
+}
+
+template <class T>
+void GenStack<T>::push() {
+
+}
+
+template <class T>
+T GenStack<T>::pop() {
+
+}
+
+template <class T>
+T GenStack<T>::peek() {
+
+}
+
+template <class T>
+int GenStack<T>::isFull() {
+
+}
+
+template <class T>
+int GenStack<T>::isEmpty() {
+
+}
+
+template <class T>
+void GenStack<T>::sourceCodePath() {
+    
 }
