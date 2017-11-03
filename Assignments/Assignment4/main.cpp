dabb3bb620b5a6fd96476498258ccb7b426f0f94
Zach Jagoda <jagod101@mail.chapman.edu>
2017-11-03 16:29:04 -0700

    [UPDATE] breaking the law part 3

--- Assignments/Assignment4/main.cpp
+++ Assignments/Assignment4/main.cpp
@@ -9,14 +9,15 @@ Assignment 4 - Registrar’s Office Simulation
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "Simulate.cpp"
+#include "assign4.cpp"
 
 using namespace std;
 
 int main(int argc, char** argv) {
     //INSERT CONSTRUCTOR
-    Operation o;
-    
+    //Operation o;
+    Simulation s;
+
     //If no file specified when running a.out
     if(argc < 2) {
         cout << "ERROR: Please Enter Valid File Input\n" << endl;
@@ -25,7 +26,63 @@ int main(int argc, char** argv) {
     //If file is specified when running a.out
     else if(argc >= 2) {
         string file = argv[1];
-        o.runSimulation(file);
+        
+        int currTime = 0;
+        int openWindows = 0;
+    
+        bool running = true;
+    
+        s.importFile(file);
+    
+        while(running) {
+            if(s.studentQueue.isEmpty()) {
+                int temp = 0;
+    
+                for(int i = 0; i < s.totalWindows; ++i) {
+                    if((s.windowArray[i]->windowTime) < 1) {
+                        temp++;
+                    }
+                }
+                if(temp = s.totalWindows) {
+                    running = false;
+                }
+            }
+            for(int i = 0; i < s.totalWindows; ++i) {
+                if(s.windowArray[i]->windowTime < 1) {
+                    if(!s.studentQueue.isEmpty()) {
+                        Students* student = s.studentQueue.vFront();
+    
+                        if(student->entryTime <= currTime) {
+                            if(s.windowArray[i]->idleTime > 0) {
+                                s.idleStats.enqueue(s.windowArray[i]->idleTime);
+                            }
+                            //Move from Queue to Window Array
+                            s.windowArray[i] = s.studentQueue.dequeue();
+                            s.waitStats.enqueue(s.windowArray[i]->queueTime);
+                        }
+                    }
+                }
+            }
+            currTime++;
+            s.timeTracker(currTime);
+        }
+    
+        for(int i = 0; i < s.totalWindows; ++i) {
+            if(s.windowArray[i]->idleTime > 0) {
+                s.idleStats.enqueue(s.windowArray[i]->idleTime);
+            }
+        }
+    
+        cout << "\n<------------SIMULATION COMPLETE------------>" << endl;
+        cout << "STUDENT Calculations" << endl;
+        cout << "Mean Student Wait Time: " << s.meanTime() << endl;
+        cout << "Median Student Wait Time: " << s.medianTime() << endl;
+        cout << "Longest Student Wait Time: " << s.longestTime() << endl;
+        cout << "Students Waiting Over Ten Minutes: " << s.overTenTime() << endl;
+        cout << "\nWINDOW Calculations" << endl;
+        cout << "Mean Window Idle Time: " << s.meanIdleTime() << endl;
+        cout << "Longest Window Idle Time: " << s.longestIdleTime() << endl;
+        cout << "Idle Time Over Five Minutes: " << s.idleOverFiveTime() << endl;
     }
     return 0;
 }
