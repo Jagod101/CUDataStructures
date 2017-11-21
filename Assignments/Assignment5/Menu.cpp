cad3766e41240da2052341cad146dcb5985c5831
Zach Jagoda <jagod101@mail.chapman.edu>
2017-11-20 22:36:54 -0800

    [TEST] Possible Fix for Infinite Loop

--- Assignments/Assignment5/Menu.cpp
+++ Assignments/Assignment5/Menu.cpp
@@ -44,7 +44,8 @@ void Menu::printMenu() {
 
     if(!cin || input < 0 || input > 14) {
       cout << "Glitch Within the Matrix - Please Enter a Valid Menu Option \n" << endl;
-      input = 0;
+      cin.sync();
+      cin.clear();
       continue;
     }
 
